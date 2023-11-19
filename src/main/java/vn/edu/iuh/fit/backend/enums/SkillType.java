ae2b64ed9d17713a0c99de8ffc97c68aa2ae6d28
PhamChiXuan1 <99774231+PhamChiXuan1@users.noreply.github.com>
2023-11-19 22:37:11 +0700

    create project, structure

---
+++ src/main/java/vn/edu/iuh/fit/backend/enums/SkillType.java
@@ -0,0 +1,11 @@
+package vn.edu.iuh.fit.backend.enums;
+
+public enum SkillType {
+    UNSPECIFIC(0),
+    TECHNICAL_SKILL(1),
+    SOFT_KILL(2)
+    ;
+
+    SkillType(int value) {
+    }
+}
---
+++ src/main/java/vn/edu/iuh/fit/backend/models/Company.java
@@ -0,0 +1,35 @@
+package vn.edu.iuh.fit.backend.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.checkerframework.checker.units.qual.C;
+
+import java.util.List;
+@Entity
+@Table(name = "company")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "com_id")
+    private long id;
+    @Column(name = "comp_name", nullable = false)
+    private String name;
+    @OneToOne
+    @JoinColumn(name = "address", nullable = false)
+    private Address address;
+    @Column(name = "web_url")
+    private String webURL;
+    @Column(name = "phone", nullable = false)
+    private String phone;
+    @Column(name = "email", nullable = false)
+    private String email;
+    @Column(name = "about", length = 2000)
+    private String about;
+    @OneToMany(mappedBy = "company")
+    private List<Job> jobs;
+}
